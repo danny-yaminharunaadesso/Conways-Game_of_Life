55bcb0110b4b7010c8fba6b04655df02e29c8eb1
Wagner <david.wagner@adesso.de>
2025-02-26 11:29:23 +0100

    Created Timer Class With getter and setter

--- Conways-Game_of_Life-DnD/src/Timer.java
+++ Conways-Game_of_Life-DnD/src/Timer.java
@@ -1,4 +1,18 @@
 
 public class Timer {
-
+	
+	private int seconds;
+	
+	public Timer(int seconds) {
+		this.seconds = seconds;
+	}
+	
+	public int getSeconds() {
+		return this.seconds;
+	}
+	
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+	
 }
