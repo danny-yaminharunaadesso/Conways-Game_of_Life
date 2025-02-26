7236e69834fcce9ad9056982cb32e4ab53781f61
Wagner <david.wagner@adesso.de>
2025-02-26 13:34:58 +0100

    create playground

--- Conways-Game_of_Life-DnD/src/Cell.java
+++ Conways-Game_of_Life-DnD/src/Cell.java
@@ -1,4 +1,16 @@
 
 public class Cell {
+	private boolean alive;
 	
+	public Cell(boolean alive) {
+		this.alive = alive;
+	}
+	
+	public boolean getLife() {
+		return this.alive;
+	}
+	
+	public void setLife (boolean alive) {
+		this.alive = alive;
+	}
 }
--- Conways-Game_of_Life-DnD/src/Playground.java
+++ Conways-Game_of_Life-DnD/src/Playground.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Playground {
 
+	private int xSize;
+	private int ySize;
+	
+	private Cell[][] cellMap = new Cell[10][10];
+	
+	public Playground(int xSize, int ySize) {
+		this.xSize = xSize;
+		this.ySize = ySize;
+	}
+	
+	public int getXSize() {
+		return this.xSize;
+	}
+	
+	public int getYSize() {
+		return this.ySize;
+	}
+	
+	public Cell[][] getCellMap() {
+		return this.cellMap;
+	}
+	
+	public void setYSize(int ySize) {
+		this.ySize = ySize;
+	}
+	
+	public void setXSize(int xSize) {
+		this.xSize = xSize;
+	}
 }
