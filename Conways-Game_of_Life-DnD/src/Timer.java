4a05a208c2bd882e6869744b1667f4bd9583494e
Wagner <david.wagner@adesso.de>
2025-02-26 11:37:57 +0100

    Added wait method to Timer

--- Conways-Game_of_Life-DnD/src/Timer.java
+++ Conways-Game_of_Life-DnD/src/Timer.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit;
 
 public class Timer {
 	
@@ -13,6 +14,14 @@ public class Timer {
 	
 	public void setSeconds(int seconds) {
 		this.seconds = seconds;
+	}
+	
+	public void waitSeconds() {
+		try {
+			TimeUnit.SECONDS.sleep(this.seconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
