ed95f0cea3b133c3988ace986b2c48a90eda3f70
Wagner <david.wagner@adesso.de>
2025-02-28 07:56:00 +0100

    Added 20 Cells Alife and fill cellMap

---
+++ Conways-Game_of_Life-DnD/src/CellLogic.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class CellLogic {
+	
+	Random random = new Random();
+	
+	public void fillCellMap(Cell[][] cellMap) {
+		for (int x = 0; x < cellMap.length; x++) {
+			for (int y = 0; y < cellMap[0].length; y++) {
+				cellMap[x][y] = new Cell(false);
+			}
+		}
+	}
+	
+	public void printCellAlife (Cell[][] cellMap) {
+		for (int x = 0; x < cellMap.length; x++) {
+			for (int y = 0; y < cellMap[0].length; y++) {
+				System.out.print(cellMap[x][y].getLife());
+			}
+			System.out.println();
+		}
+	}
+	
+	public void setTwentyCellsAlive(Cell[][] cellMap) {
+		int x = 0;
+		int y = 0;
+		
+		for (int i = 0; i < 20; i++) {
+			
+			while (true) {
+				y = random.nextInt(0,cellMap.length);
+				x = random.nextInt(0,cellMap[0].length);
+				
+				if (!cellMap[x][y].getLife()) {
+					break;
+				}			
+			}
+			
+			cellMap[x][y].setLife(true);
+			
+		}
+		
+	}
+	
+}
--- Conways-Game_of_Life-DnD/src/Controller.java
+++ Conways-Game_of_Life-DnD/src/Controller.java
@@ -2,12 +2,15 @@
 public class Controller {
  
 	Input input = new Input();
+	CellLogic cellLogic = new CellLogic();
 	
 	Playground playground = new Playground(input.getXWayDefaultValue(), input.getYWayDefaultValue());
 	
 	public void start() {
 		System.out.println(playground.getCellMap()[0].length);
- 
+		cellLogic.fillCellMap(playground.getCellMap());
+		cellLogic.setTwentyCellsAlive(playground.getCellMap());
+		cellLogic.printCellAlife(playground.getCellMap());
 	}
 	
 }
