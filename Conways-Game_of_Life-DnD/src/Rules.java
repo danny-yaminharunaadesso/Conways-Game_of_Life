3db58359a96abb3e887504a88d5dc23be5c0c2ef
Wagner <david.wagner@adesso.de>
2025-03-03 15:43:45 +0100

    neu:D

--- Conways-Game_of_Life-DnD/src/Rules.java
+++ Conways-Game_of_Life-DnD/src/Rules.java
@@ -2,8 +2,8 @@
 public class Rules {
 	
 	public void setCellAlifeOrDead(Cell[][] cellMap, boolean[][] booleanMap) {
-		for (int y = 0; y < cellMap.length-1; y++) {
-			for (int x = 0; x < cellMap[0].length-1; x++) {
+		for (int y = 0; y < cellMap.length; y++) {
+			for (int x = 0; x < cellMap[0].length; x++) {
 				if (booleanMap[y][x]) {
 					cellMap[y][x].setLife(true);
 				} else {
@@ -14,8 +14,8 @@ public class Rules {
 	}
 	
 	public void twoOrLessNeighbours(Cell[][] cellMap, boolean[][] booleanMap) {
-		for (int y = 0; y < cellMap.length-1; y++) {
-			for (int x = 0; x < cellMap[0].length-1; x++) {
+		for (int y = 0; y < cellMap.length; y++) {
+			for (int x = 0; x < cellMap[0].length; x++) {
 				if (cellMap[y][x].getNeighbours() < 2) {
 					booleanMap[y][x] = false;
 				}
@@ -24,8 +24,8 @@ public class Rules {
 	}
 	
 	public void moreThanThreeNeighbours(Cell[][] cellMap, boolean[][] booleanMap) {
-		for (int y = 0; y < cellMap.length-1; y++) {
-			for (int x = 0; x < cellMap[0].length-1; x++) {
+		for (int y = 0; y < cellMap.length; y++) {
+			for (int x = 0; x < cellMap[0].length; x++) {
 				if (cellMap[y][x].getNeighbours() > 3) {
 					booleanMap[y][x] = false;
 				}
@@ -34,13 +34,33 @@ public class Rules {
 	}
 	
 	public void threeNeighbours(Cell[][] cellMap, boolean[][] booleanMap) {
-		for (int y = 0; y < cellMap.length-1; y++) {
-			for (int x = 0; x < cellMap[0].length-1; x++) {
+		for (int y = 0; y < cellMap.length; y++) {
+			for (int x = 0; x < cellMap[0].length; x++) {
 				if (cellMap[y][x].getNeighbours() == 3) {
 					booleanMap[y][x] = true;
 				}
 			}
 		}
+	}
+	
+	public boolean endOfGame(Cell[][] cellMap) {
+		int countAlife = 0;
+		for (int y = 0; y < cellMap.length; y++) {
+			for (int x = 0; x < cellMap[0].length; x++) {
+				if (cellMap[y][x].getLife()) {
+					countAlife++;
+				}
+				
+			}
+		}
+		
+		if (countAlife == cellMap.length * cellMap[0].length) {
+			return true;
+		} else if (countAlife == 0) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
