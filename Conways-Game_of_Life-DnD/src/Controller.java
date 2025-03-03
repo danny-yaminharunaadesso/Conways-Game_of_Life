859855ee733d1b28dcd8c1f232a8c488eb0b22a4
Wagner <david.wagner@adesso.de>
2025-03-03 09:13:12 +0100

    Added rule functions

--- Conways-Game_of_Life-DnD/src/Controller.java
+++ Conways-Game_of_Life-DnD/src/Controller.java
@@ -1,8 +1,8 @@
-
 public class Controller {
  
 	Input input = new Input();
 	CellLogic cellLogic = new CellLogic();
+	Timer timer = new Timer(3);
 	
 	Playground playground = new Playground(input.getXWayDefaultValue(), input.getYWayDefaultValue());
 	PlaygroundLogic playgroundLogic = new PlaygroundLogic();
@@ -11,7 +11,15 @@ public class Controller {
 		System.out.println(playground.getCellMap()[0].length);
 		cellLogic.fillCellMap(playground.getCellMap());
 		cellLogic.setTwentyCellsAlive(playground.getCellMap());
-		cellLogic.printCellAlife(playground.getCellMap());
+		while (true) {
+			cellLogic.printCellAlife(playground.getCellMap());
+			cellLogic.countCellNeighbours(playground.getCellMap());
+			
+			cellLogic.twoOrLessNeighbours(playground.getCellMap());
+			cellLogic.moreThanThreeNeighbours(playground.getCellMap());
+			cellLogic.threeNeighbours(playground.getCellMap());
+			timer.waitSeconds();
+		}
 	}
 	
 }
