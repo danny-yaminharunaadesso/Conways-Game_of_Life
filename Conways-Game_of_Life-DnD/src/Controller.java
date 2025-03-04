9604d45ef6ffb227896ad9a69151afcc2efa0c31
Danny Yamin Haruna <danny-yamin.haruna@adesso.de>
2025-03-04 11:20:53 +0100

    11:19

--- Conways-Game_of_Life-DnD/src/Controller.java
+++ Conways-Game_of_Life-DnD/src/Controller.java
@@ -7,7 +7,10 @@ public class Controller {
 	Playground playground = new Playground(input.getXWayDefaultValue(), input.getYWayDefaultValue());
 	PlaygroundLogic playgroundLogic = new PlaygroundLogic();
 	
+	
 	public void start() {
+		
+		
 		System.out.println(playground.getCellMap()[0].length);
 		cellLogic.fillCellMap(playground.getCellMap());
 		cellLogic.setTwentyCellsAlive(playground.getCellMap());
--- Conways-Game_of_Life-DnD/src/StopandPause.java
+++ Conways-Game_of_Life-DnD/src/StopandPause.java
@@ -1,49 +1,34 @@
 import java.util.Scanner;
 
-public class StopandPause extends Thread{
-	
-	@Override
-	public void run() {
-		Scanner scannerStartStop = new Scanner (System.in);
-		String input = scannerStartStop.nextLine();
-		
-		
-		if (input.equalsIgnoreCase("p")) {
-			
-			try {
-				Scanner scannerChoice = new Scanner (System.in);
-				System.out.println("Sie haben Conway´s Game of Life pausiert wie möchten Sie fortfahren, \r\n"
-						+ "[1] Spiel fortsetzen, [2] Spiel beenden");
-				int choice = scannerChoice.nextInt();
-				if (choice == 1) {
-					stopGame();
-				}else if (choice == 2) {
-					
-				}else {
-					System.out.println("Falsche Eingabe! Versuchs nochmal!");
-					choice = scannerChoice.nextInt();
-				}
-				
-			} catch (InterrputedExeption e){
-				
-			}
-			
-			
-		}
-		
-	}
-	
-	public void stopGame() {
-		System.out.println("Das Spiel wurde beendet!");
-		System.exit(0);												//Java Programm wird beendet
-	}
+public class StopandPause extends Thread {
 
-	public void lastGeneration() {
-		// hier kann man den 2. Array holen und nochmals die letzte Ausgabe ausgeben
-			// - mit einer doppelten vorschleife, ...
-	}
-	
-	public void continueGame() {
-		// Spiel fortsetzen falls es nicht automatisch geht
-	}
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in); // Ein Scanner für alles
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("p")) {
+                System.out.println("Sie haben Conway´s Game of Life pausiert. Wie möchten Sie fortfahren?\n" +
+				        "[1] Spiel fortsetzen\n[2] Spiel beenden");
+
+				int choice = scanner.nextInt();
+				scanner.nextLine(); // Leere Zeile nach `nextInt()` konsumieren
+
+				if (choice == 1) {
+				    System.out.println("Spiel wird fortgesetzt...");
+				} else if (choice == 2) {
+				    stopGame();
+				} else {
+				    System.out.println("Falsche Eingabe! Versuche es nochmal.");
+				}
+            }
+        }
+    }
+
+    public void stopGame() {
+        System.out.println("Das Spiel wurde beendet!");
+        System.exit(0); // Java-Programm beenden
+    }
 }
